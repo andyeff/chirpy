0e694bc097af2662bb688d4647ea26f2093249db
Andy Eff <andyf@andyf.me.uk>
2024-11-04 22:25:13 +0000

    Add health check handler and change route for app

--- main.go
+++ main.go
@@ -7,9 +7,16 @@ import (
 
 const port = "8080"
 
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", healthcheck)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
