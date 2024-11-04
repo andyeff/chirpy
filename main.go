89752be0916c48922430aafb3db3b5bcc836d467
Andy Eff <andyf@andyf.me.uk>
2024-11-04 23:29:50 +0000

    middleware, sheesh

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) showrequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) resetrequests(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits reset to zero: %d", cfg.fileserverHits.Load())))
+}
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
@@ -14,9 +39,14 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", healthcheck)
+	mux.HandleFunc("/metrics", apiCfg.showrequests)
+	mux.HandleFunc("/reset", apiCfg.resetrequests)
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
